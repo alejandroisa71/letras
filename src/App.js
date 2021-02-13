d8f4ad60e85585df38570d3890bfe56440307fff
Alejandro Isa <alejandroisa@gmail.com>
2021-02-13 03:45:29 -0300

    se crea componente Info.js en donde se muestra la informacion que se consume de la API theaudiodb.com. Se puede mejorar el proyecto creando expresiones javascript fuera del return

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { computeHeadingLevel } from "@testing-library/react";
 import React, { Fragment, useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Cancion from "./components/Cancion";
+import Info from "./components/Info";
 
 import axios from "axios";
 
@@ -9,6 +10,8 @@ function App() {
   //Definir el State
   const [busquedaletra, setBusquedaLetra] = useState({});
   const [letra, setLetra] = useState("");
+  const [info, setInfo] = useState({});
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     if (Object.keys(busquedaletra).length === 0) return;
@@ -17,24 +20,52 @@ function App() {
       const { artista, cancion } = busquedaletra;
 
       const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
-      const resultado = await axios(url);
+      const [letra, informacion] = await Promise.all([axios(url), axios(url2)]);
+      if (informacion.data.artists === null) {
+        setError(true);
+        return;
+      } else {
+        setError(false);
+      }
 
-      setLetra(resultado.data.lyrics);
+      setLetra(letra.data.lyrics);
+      setInfo(informacion.data.artists[0]);
     };
     consultarAPILetra();
-  }, [busquedaletra]);
+  }, [busquedaletra, info]);
 
+  const { artista, cancion } = busquedaletra;
   return (
     <Fragment>
       <Formulario setBusquedaLetra={setBusquedaLetra} />
       <div className="container mt-5 ">
-        <div className="row">
-          <div className="col-md-6"></div>
-          <div className="col-md-6">
-            <Cancion letra={letra} />
+        {error ? (
+          <div className="row">
+            <div className="col-md-6">
+              <p className="alert alert-primary text-center">
+                No se encontró el artista
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="row">
+            <div className="col-md-6">
+              <Info info={info} />
+            </div>
+            <div className="col-md-6">
+              {letra.length === 0 && Object.keys(info).length !== 0 ? (
+                <p className="alert alert-primary text-center">
+                  No se encontró la letra de la canción {cancion} para el
+                  artista: {artista}
+                </p>
+              ) : (
+                <Cancion letra={letra} />
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </Fragment>
   );
